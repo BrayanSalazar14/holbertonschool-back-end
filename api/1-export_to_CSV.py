cfeb71df6cb12e840d2cab6356f3667b8ab0a715
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-04-29 15:12:35 -0500

    New project name API, task number 0

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,17 +8,12 @@ if __name__ == '__main__':
     usr_name = emp_data.json().get('name')
 
     tasks_data = requests.get(f'https://jsonplaceholder.typicode.com/todos')
-    comp_tasks = [data["userId"] for data in tasks_data.json()
-                  if data["completed"] is True]
 
     all_tasks = [data["userId"] for data in tasks_data.json()]
-    comp_tasks = comp_tasks.count(id)
-    all_tasks = all_tasks.count(id)
 
-    titles = [title['title'] for title in tasks_data.json()
+    task_data = requests.get(f'https://jsonplaceholder.typicode.com/todos')
+    titles = [title['title'] for title in task_data.json()
               if title['completed'] is True and title['userId'] == id]
-
-    print(f"Employee {usr_name} is done with tasks({comp_tasks}/{all_tasks}):")
 
     for title in titles:
         print("\t ", title)

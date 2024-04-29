5f5e80e5602828e5c3168ffcce262724459c44e0
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-04-29 18:55:31 -0500

    new task

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 import requests
+import csv
 from sys import argv
 
 if __name__ == '__main__':
     id = int(argv[1])
     emp_data = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
     usr_name = emp_data.json().get('name')
-
     tasks_data = requests.get(f'https://jsonplaceholder.typicode.com/todos')
+    data_all = [boole for boole in tasks_data.json()
+                if boole.get('userId') == id]
 
-    all_tasks = [data["userId"] for data in tasks_data.json()]
-
-    task_data = requests.get(f'https://jsonplaceholder.typicode.com/todos')
-    titles = [title['title'] for title in task_data.json()
-              if title['completed'] is True and title['userId'] == id]
-
-    for title in titles:
-        print("\t ", title)
+    filename = f"{id}.csv"
+    with open(filename, 'a', newline='') as csvfile:
+        write = csv.writer(csvfile, dialect="unix")
+        for data in data_all:
+            write.writerow([data['userId'], usr_name,
+                            data['completed'], data['title']])

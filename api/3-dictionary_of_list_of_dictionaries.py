1f47c46b837d907b81ef9e0ae46b5fdff75541bf
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-05-02 14:35:06 -0500

    changes for the logic code

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -8,21 +8,22 @@ import requests
 if __name__ == '__main__':
     tasks_data = requests.get(
         'https://jsonplaceholder.typicode.com/todos').json()
+
     users_id = list(set([ids['userId'] for ids in tasks_data]))
     dic = {}
-    for userId in users_id:
+    for usrId in users_id:
 
         emp_name = requests.get(
-            f'https://jsonplaceholder.typicode.com/users/{userId}').json()
+            f'https://jsonplaceholder.typicode.com/users/{usrId}').json()
 
         emp_data = requests.get(
-            f'https://jsonplaceholder.typicode.com/users/{userId}/todos').json()
+            f'https://jsonplaceholder.typicode.com/users/{usrId}/todos').json()
 
         list_data = [{
             "username": emp_name['username'],
             "task": data['title'],
             "completed:": data['completed']} for data in emp_data]
 
-        dic[userId] = list_data
+        dic[usrId] = list_data
     with open('todo_all_employees.json', 'w', encoding='utf-8') as file:
         json.dump(dic, file)
